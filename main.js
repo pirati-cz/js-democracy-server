a9b3d7b861877936da2ad273e7135aecd83ab91f
Vaclav Klecanda <vencax77@gmail.com>
2013-09-12 10:42:15 +0200

    running switch for debug env

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+if(process.env.DEVMACHINE) {
+  process.env.PORT = 8080;
+  var app = require('./tmp/app');
+} else {
+  require('coffee-script');
+  var app = require('./lib/app');
+}
 
-var app = require('./tmp/app');
-
-process.env.PORT = 8080;
 app.listen((process.env.PORT || 8080), function onListening() {
   console.log('listening at %s', process.env.PORT);
 });
