521f64fc00c5d27814ad5c32f2f334d0f3ee804b
Vaclav Klecanda <vencax77@gmail.com>
2014-01-28 20:55:09 +0100

    main more verbose, absolute imports

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 if(process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
   require('coffee-script');
-  var app = require('./lib/app');
+  var app = require(__dirname +'/lib/app');
 } else {
   process.env.PORT = 3000;
-  var app = require('./target/app');
+  process.env.NODE_ENV = 'devel';
+  var app = require(__dirname + '/target/app');
 }
 
 var port = process.env.PORT || 8080;
 
-console.log('starting at port: %s', port);
+console.log('starting in %s, at %s, port: %s',
+  __dirname, process.env.NODE_ENV, port);
 
 app.listen(port, function() {
   console.log('listening ...');
