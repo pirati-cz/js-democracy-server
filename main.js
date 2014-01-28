d6b6653b22f8809149fa33aab70a43d5450f158c
Vaclav Klecanda <vencax77@gmail.com>
2014-01-28 20:38:45 +0100

    in production running from coffeess again (no autocompile on slug compilation yet)

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-
-var app = require('./target/app');
+if(process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
+  require('coffee-script');
+  var app = require('./lib/app');
+} else {
+  process.env.PORT = 3000;
+  var app = require('./target/app');
+}
 
 var port = process.env.PORT || 8080;
 
