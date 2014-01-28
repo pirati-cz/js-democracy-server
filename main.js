c719655e72887d882e0f6483d9663fc2411fb53a
Vaclav Klecanda <vencax77@gmail.com>
2014-01-28 19:53:00 +0100

    change of main

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 var app = require('./dist/app');
 
-app.listen((process.env.PORT || 8080), function () {
-  console.log('listening at %s', process.env.PORT);
+var port = process.env.PORT || 8080;
+
+console.log('starting at port: %s', port);
+
+app.listen(port, function() {
+  console.log('listening ...');
 });
